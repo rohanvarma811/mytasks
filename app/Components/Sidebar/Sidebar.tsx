af391c08c7fb673d3d366ace2709c150c89aae24
Rohan Varma <rohanvarma911@gmail.com>
2024-09-28 23:56:28 +0530

    Fixing Sidebar

--- app/Components/Sidebar/Sidebar.tsx
+++ app/Components/Sidebar/Sidebar.tsx
@@ -1,8 +1,22 @@
 "use client"
 import React from "react";
+import styled from "styled-components";
+import { useGlobalState } from "@/app/context/golbalProvider";
 
 function Sidebar() {
-  return <div>Sidebar</div>;
+
+  const theme  = useGlobalState();
+  console.log(theme);
+
+  return <SidebarStyled>Sidebar</SidebarStyled>;
 }
+
+const SidebarStyled = styled.nav`
+  position: relative;
+  width: ${(props) => props.theme.sidebarWidth};
+  background-color: ${(props) => props.theme.colorBg2};
+  border-right: 2px solid ${(props) => props.theme.borderColor2};
+  border-radius: 1rem;
+`;
 
 export default Sidebar;
