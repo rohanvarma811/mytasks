d5de6e503249cdc58a7d62d821d26b77e56c91af
Rohan Varma <rohanvarma911@gmail.com>
2024-10-04 18:08:49 +0530

    Checking Sidebar

--- app/Components/Sidebar/Sidebar.tsx
+++ app/Components/Sidebar/Sidebar.tsx
@@ -4,18 +4,17 @@ import styled from "styled-components";
 import { useGlobalState } from "@/app/context/golbalProvider";
 
 function Sidebar() {
+  const { theme } = useGlobalState();
 
-  const theme  = useGlobalState();
-  console.log(theme);
-
-  return <SidebarStyled>Sidebar</SidebarStyled>;
+  return <SidebarStyled theme={theme}>Sidebar</SidebarStyled>;
 }
 
 const SidebarStyled = styled.nav`
   position: relative;
   width: ${(props) => props.theme.sidebarWidth};
   background-color: ${(props) => props.theme.colorBg2};
-  border-right: 2px solid ${(props) => props.theme.borderColor2};
+  border: 2px solid ${(props) => props.theme.borderColor2};
+
   border-radius: 1rem;
 `;
 
--- app/providers/ContextProvider.tsx
+++ app/providers/ContextProvider.tsx
@@ -1,29 +1,27 @@
 "use client";
 import React from "react";
-import GlobalStyleProvider from "./GlobalStyleProvider";
 import { GlobalProvider } from "../context/golbalProvider"
-// import { Toaster } from "react-hot-toast";
 
 interface Props {
   children: React.ReactNode;
 }
 
 function ContextProvider({ children }: Props) {
-    // const [isReady, setIsReady] = React.useState(false);
+    const [isReady, setIsReady] = React.useState(false);
 
-    // React.useEffect(() => {
-    //     setTimeout(() => {
-    //     setIsReady(true);
-    //     }, 250);
-    // }, []);
+    React.useEffect(() => {
+        setTimeout(() => {
+        setIsReady(true);
+        }, 250);
+    }, []);
 
-    // if (!isReady) {
-    //     return (
-    //         <div className="w-full h-full flex items-center justify-center">
-    //             <span className="loader"></span>
-    //         </div>
-    //     );
-    // }
+    if (!isReady) {
+        return (
+            <div className="w-full h-full flex items-center justify-center">
+                <span className="loader"></span>
+            </div>
+        );
+    }
 
     return (
         <GlobalProvider>
