40e0509bb7a9b6b76a293d919349948269c61b11
Rohan Varma <rohanvarma911@gmail.com>
2024-10-07 06:03:08 +0530

    Commenting code

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -4,6 +4,7 @@ import React from "react";
 
 function Page() {
   return (
+    // HERE THE ROUTING ISSUE IS FIXED DURING LOGIN
     <div className="flex items-center justify-center h-full">
       <SignIn routing="hash" />
     </div>
