a7bd6580b01ea0ec0bc44fb94d55e057017312f1
Rohan Varma <rohanvarma911@gmail.com>
2024-10-08 03:25:56 +0530

    Fixing Minor Bugs

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,26 @@
 "use client";
 import Tasks from "./Components/Tasks/Tasks";
 import { useGlobalState } from "./context/golbalProvider";
+import { useAuth } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
   const { tasks } = useGlobalState();
+  const { isSignedIn } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    // Redirect to the sign-in page if the user is not authenticated
+    if (!isSignedIn) {
+      router.push("/signin");
+    }
+  }, [isSignedIn, router]);
+
+  // Show tasks only if the user is signed in
+  if (!isSignedIn) {
+    return null; // or a loading spinner can be shown here while redirecting
+  }
 
   return <Tasks title="All Tasks" tasks={tasks} />;
 }
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -5,7 +5,7 @@ import React from "react";
 function page() {
   return (
     <div className="flex items-center justify-center h-full">
-      <SignUp />
+      <SignUp routing="hash" />
     </div>
   );
 }
